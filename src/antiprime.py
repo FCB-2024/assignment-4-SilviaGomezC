4887f82e1f1c9a07553a7a6c9b8cef1f09617334
Sillvia Gomez <silvia.gomez09@estudiant.upf.edugit config --global user.email silvia.gomez09@estudiant.upf.edu>
2024-10-21 21:18:02 +0200

    Afegir python antiprime or not antiprimer 1

--- src/antiprime.py
+++ src/antiprime.py
@@ -2,6 +2,7 @@
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
 def main(x):
+  ## YOU CODE SHOULD START HERE AST THE SAME
     # Encontrar los divisores de x
     r = 0
     y = 1
@@ -28,6 +29,7 @@ def main(x):
     # Imprimir resultado
     print("anti-prime")
 
+## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__":
     # Definición de la variable x con input del usuario
     x = int(input("Enter a positive integer number: "))
@@ -35,4 +37,14 @@ if __name__ == "__main__":
 main(x)
 
 
-
+	## IDENTATION AS THIS COMMENT
+	## THE LAST LINES OF YOUR CODE SHOULD EITHER
+	## RETURN THE VALUE "anti-prime" or "not anti-prime"
+	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
+	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
+	## "anti-prime" or "not anti-prime
+	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
+	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
+	## $ python antiprime.py 6
+	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
+	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
