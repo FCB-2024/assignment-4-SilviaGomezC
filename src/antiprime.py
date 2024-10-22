3d2fee2c9e43298568bc96f8da6ed910c641cbff
SilviaGomezC <silvia.gomez09@estudiant.upf.edu>
2024-10-22 15:41:31 +0200

    Programa python antiprime

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,49 +1,29 @@
-## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
-## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
-## COMMAND LINE SPECIFIED BELOW
-def main(x):
-  ## YOU CODE SHOULD START HERE AST THE SAME
-    # Encontrar los divisores de x
+import sys
+
+def sum_divisors(x):
     r = 0
     y = 1
     while y <= x:
         if x % y == 0:
             r += 1
         y += 1
+    return r
 
+def main(x):
+    # Encontrar los divisores de x
+    c = sum_divisors(x)
     # Comparación con los números inferiores
-    max_divisors = r
-    i = 1
-    while i < x:
-        r_i = 0
-        y = 1
-        while y <= i:
-            if i % y == 0:
-                r_i += 1
-            y += 1
-        if r_i >= max_divisors:
+    max_divisors = c
+    l = x - 1
+    while l >= 1:
+        if sum_divisors(l) >= max_divisors:
             print("not anti-prime")
-            return    
-        i += 1
+            return
+        l -= 1
 
-    # Imprimir resultado
     print("anti-prime")
 
-x = int(input("Enter a positive integer number: "))
-   
-## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__":
-    # Definición de la variable x con input del usuario
-    # Llamada a main(x)
+    x = int(sys.argv[1])
     main(x)
-	## IDENTATION AS THIS COMMENT
-	## THE LAST LINES OF YOUR CODE SHOULD EITHER
-	## RETURN THE VALUE "anti-prime" or "not anti-prime"
-	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
-	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
-	## "anti-prime" or "not anti-prime
-	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
-	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
-	## $ python antiprime.py 6
-	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
-	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
+
